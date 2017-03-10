62e52ca9e0ea899e5685363c8f32c12f7f06cb4b
AnnaZubareva <gradarikan@gmail.com>
2017-03-11 02:54:29 +0400

    основные страницы

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+
+<html lang="ru">
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Main</title>
+
+<link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+
+<link rel="stylesheet" type="text/css" media="print" href="css/print.css"  />
+
+<link rel="stylesheet" type="text/css" media="screen" href="css/prettyPhoto.css" />
+
+<!--<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:light' rel='stylesheet' type='text/css'>
+
+<link href='http://fonts.googleapis.com/css?family=Just+Another+Hand' rel='stylesheet' type='text/css'>
+
+<!--[if lt IE 7]><link rel="stylesheet" type="text/css" media="screen" href="css/ie-fix.css" /><![endif]-->
+
+<script type="text/javascript" src="js/jquery-1.5.2.min.js"></script>
+
+<script type="text/javascript" src="js/jquery.tweet.js"></script>
+
+<script type="text/javascript" src="js/jquery.ScrollTo.js"></script>
+
+<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+
+<script type="text/javascript" src="js/jquery.quicksand.js"></script>
+
+<script type="text/javascript" src="js/jquery.quicksand-config.js"></script>
+
+<script type="text/javascript" src="js/scripts.js"></script> 
+
+</head>
+<body>
+
+<div id="page-shadow">
+
+	<div id="page">
+	
+		<div class="content-innertube">
+	
+			<div id="header">
+			
+  <h1><img src="http://x-lines.ru/letters/i/cyrillicscript/3091/000000/56/0/4np7bqgto9em5wcm4nh1bwcb4napbqqtoeopbf6toxemdwfo4gypbpqosmem7wf3rdejbwf74n67dna.png">
+</h1>
+				
+
+				
+				
+				<ul id="main-nav">
+                                        <li><a href="index.php"><img src="http://x-lines.ru/letters/i/cyrillicbasic/0597/000000/18/0/4nj7bq6osdemfwf74napdda.png"></a></li>
+					<li><a href="about.html"><img src="http://x-lines.ru/letters/i/cyrillicbasic/0597/000000/18/0/4nxpbcqozaopbxgozzemk.png">
+</a></li>
+                                        <li><a href="portfolio.html"><img src="http://x-lines.ru/letters/i/cyrillicbasic/0597/000000/18/0/4nj7bcgozxemmwcy4n47dda.png">
+</a></li>
+					<li><a href="contact.php"><img src="http://x-lines.ru/letters/i/cyrillicbasic/0597/000000/18/0/4nopbpqosxemtwcb4gbpdygosdeapwfa4g819wr14gn7bxsoso.png">
+</a></li>
+				    
+				</ul>
+				
+				<div class="clear"></div>
+				
+			</div><!-- header end -->
+			
+			
+			
+		
+			<div id="content-left">
+			
+				<div class="section">
+			
+					<div class="clearfix"><h2>Персональные данные</h2></div>
+					
+					<div class="item">
+					
+						<ul id="personal-details">
+							<li><span>Имя:</span>Зубарева Анна</li>
+							<li><span>Дата рождения:</span>2 января 1999</li>
+							<li class="clearfix"><span>Образование:</span>
+								<ul id="address-lines">
+									<li>УЛГТУ,</li>
+									<li>ФИСТ,</li>
+									<li>ИВТВМбд-11</li>
+								</ul>
+							</li>
+						        <li><span>Email:</span><a href="mailto:#">gradarikan@gmail.com</a></li>
+						</ul>
+						
+					</div><!-- item end -->
+				
+				</div><!-- section end -->
+				
+				
+				<div class="section">
+			
+					<div class="clearfix"><h2>Обратная связь</h2></div>
+					
+					<div class="item">
+				         
+					<ul>
+						<li><a href="#" title=""><img src="img/twitter.png" alt="twitter" title="My Twitter profile"></a>
+						<<a href="#" title=""><img src="img/facebook.png" alt="facebook" title="My Facebook profile"></a>
+						<a href="#" title=""><img src="img/flickr.png" alt="flickr" title="My Flickr profile"></a>
+						<a href="#" title=""><img src="img/linkedin.png" alt="linkedin" title="My Linkedin profile"></a></li>
+					</ul>
+						
+						
+					</div><!-- item end -->
+					
+					<div class="item last">
+					
+						
+					</div><!-- item end -->
+				
+				</div><!-- section end -->
+				
+			</div><!-- content-left end -->
+			
+			<div id="content-right">
+			
+				<div class="section">
+			
+                                    <h3><p><img src="img/ZMXNeeqirus.jpg" width="350" height="450"></p></h3>
+					
+					
+			</div><!-- item end -->
+					
+					
+				
+				</div><!-- section end -->
+				
+				
+				
+				<div class="section">
+			
+					
+		
+		</div><!-- content-innertube end -->
+		
+		<div class="clear"></div>
+		
+		
+
+</body>
+</html>
+
